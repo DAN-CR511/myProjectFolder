10c25841f6c0fadba59523039570c7bb3516b989
Logic Basele Labs <logicbaselabs@gmail.com>
2026-02-05 18:06:21 +0100

    committing recent changes made

---
+++ kanbanboardfolder/script.js
@@ -0,0 +1,48 @@
+const cards = document.querySelectorAll(".cards");
+const list = document.querySelectorAll(".list");
+
+for(const card of cards){
+    card.addEventListener("dragstart",dragStart);
+    card.addEventListener("dragend",dragEnd);
+}
+
+for(const list of lists) {
+    list.addEventListener("dragover",dragOver);
+    list.addEventListener("dragenter",dragEnter);
+    list.addEventListener("dragleave",dragLeave);
+    list.addEventListener("drop",dragDrop);
+}
+
+//THIS IS ALLOWS THE DROP LOCATION TO KNOW WHICH ELEMENT IS BEING MOVED WHEN YOU RELEASE IT.
+function dragStart(e) {
+    e.dataTransfer.setData("text/plain",this.id);
+}
+
+function dragEnd() {
+    console.log("Drag ended");
+}
+
+function dragOver(e) {
+    //THIS LINE IS IMPORTANT BECAUSE BY DEFAULT, BROWSERS DON'T ALLOW YOU TO DROP ELEMENTS ONTO OTHER ELEMENTS.
+
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.classList.add("over");
+}
+
+function dragLeave(e) {
+    this.classList.remove("over");
+}
+
+function dragDrop(e){
+    const id = e.dataTransfer.getData("text/plain");
+
+    const card = document.getElementById(id);
+
+    this.appendChild(card);
+
+    this.classList.remove("over");
+}
