b52b13f5851ca50317ed65f747ffe5da971c8a28
Logic Basele Labs <logicbaselabs@gmail.com>
2026-02-07 14:11:39 +0100

    Updating new staged files

--- kanbanboardfolder/script.js
+++ kanbanboardfolder/script.js
@@ -1,21 +1,22 @@
 const cards = document.querySelectorAll(".cards");
-const list = document.querySelectorAll(".list");
+const lists = document.querySelectorAll(".list");
 
-for(const card of cards){
-    card.addEventListener("dragstart",dragStart);
-    card.addEventListener("dragend",dragEnd);
+for(const card of cards) {
+    card.addEventListener("dragstart", dragStart);
+    card.addEventListener("dragend", dragEnd);
 }
 
 for(const list of lists) {
-    list.addEventListener("dragover",dragOver);
-    list.addEventListener("dragenter",dragEnter);
-    list.addEventListener("dragleave",dragLeave);
-    list.addEventListener("drop",dragDrop);
+    list.addEventListener("dragover", dragOver);
+    list.addEventListener("dragenter", dragEnter);
+    list.addEventListener("dragleave", dragLeave);
+    list.addEventListener("drop", dragDrop);
 }
 
-//THIS IS ALLOWS THE DROP LOCATION TO KNOW WHICH ELEMENT IS BEING MOVED WHEN YOU RELEASE IT.
 function dragStart(e) {
-    e.dataTransfer.setData("text/plain",this.id);
+//THIS IS ALLOWS THE DROP LOCATION TO KNOW WHICH ELEMENT IS BEING MOVED WHEN YOU RELEASE IT.
+
+    e.dataTransfer.setData("text/plain", this.id);
 }
 
 function dragEnd() {
@@ -37,12 +38,11 @@ function dragLeave(e) {
     this.classList.remove("over");
 }
 
-function dragDrop(e){
+function dragDrop(e) {
     const id = e.dataTransfer.getData("text/plain");
 
     const card = document.getElementById(id);
 
     this.appendChild(card);
-
     this.classList.remove("over");
 }
